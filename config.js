3c89f90a2eed1a1c27d7c882023ddf01d792beb1
Michael Aufreiter <michael@substance.io>
2015-02-19 14:37:54 +0100

    Use new ES instance.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  host: 'https://7zepvxjw:sqnl0xez3nmesypj@rowan-7428284.us-east-1.bonsai.io',
-  // host: 'http://192.168.33.10:9200'
+  // host: 'https://7zepvxjw:sqnl0xez3nmesypj@rowan-7428284.us-east-1.bonsai.io', // Old Host!
+  host: 'https://5vh18c43:yh79uny7q6gv658x@oak-9242088.eu-west-1.bonsai.io/'
 };
