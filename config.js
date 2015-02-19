178c043556eaea5011edc2edf90a16834ad3cb25
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-02-19 16:06:57 +0100

    Config.

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 
 var config = {
-  // host: 'https://38d8580ce27602d1000.qbox.io',
   host: 'https://iq06bv1q:6ck6kz4f28n7xsoo@fir-7016503.us-east-1.bonsai.io',
-  // host: 'http://192.168.33.10:9200'
 };
 
 console.log();
