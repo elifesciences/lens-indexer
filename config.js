f5ce6039c7951d90b3e8f6ad1f5f178ef19d4e37
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-02-19 16:00:08 +0100

    Configure ES host.

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {
   // host: 'https://38d8580ce27602d1000.qbox.io',
-  host: 'https://7zepvxjw:sqnl0xez3nmesypj@rowan-7428284.us-east-1.bonsai.io',
+  host: 'https://r0tyz8fe:39n5s8th0hioocz7@cypress-3101706.us-east-1.bonsai.io/',
+  // host: 'https://5vh18c43:yh79uny7q6gv658x@oak-9242088.eu-west-1.bonsai.io/'
   // host: 'http://192.168.33.10:9200'
 };
 
