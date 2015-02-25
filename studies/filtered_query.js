8aadf9b43a4eefa7b7bd81cbe9e2c2299581b317
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-02-25 21:16:13 +0100

    More sample scripts.

---
+++ studies/filtered_query.js
@@ -0,0 +1,68 @@
+var elasticsearch = require('elasticsearch');
+var config = require('../config');
+
+var client = new elasticsearch.Client(config);
+
+var searchString =  process.argv.slice(2).join(' ') || 'vivo'
+
+console.log('# Searching for: %s', searchString);
+
+client.search({
+  index: 'articles',
+  type: 'article',
+  body: {
+    "size": 30,
+    "sort": [
+      { "_score": { "order": "desc" } }
+    ],
+    "query": {
+      "filtered": {
+        "query": {
+          "bool": {
+            "should": [
+              { "has_child": {
+                  "type": "fragment",
+                  "score_mode" : "sum",
+                  "query": {
+                    "match": {
+                      "content": { "query": searchString, "minimum_should_match": "75%" }
+                    }
+                  }
+                }
+              },
+              {
+                "match": {
+                  "title": { "query": searchString, "minimum_should_match": "75%", "boost": 3.0 }
+                }
+              }
+            ]
+          }
+        },
+        "filter": {
+          "bool": {
+            "should": [
+              { "term": { "subjects": "Biochemistry" }},
+              { "term": { "subjects": "Cell bioligy" }}
+            ]
+          }
+        }
+      }
+    },
+    // highlight can not be combined with a filtered query
+    "highlight": {
+      "pre_tags" : ['<span class="query-string">'], "post_tags" : ["</span>"],
+      "fields": {
+        // NOTE: "number_of_fragments" : 0 is necessary to suppress lucene's automatic truncation of fragments
+        "title": { "number_of_fragments" : 0 },
+        "intro": { "number_of_fragments" : 0 }
+      }
+    }
+  }
+}).then(function (body) {
+  var hits = body.hits.hits;
+  console.log(JSON.stringify(body, null, 2));
+  client.close();
+}, function (error) {
+  console.trace(error.message);
+  client.close();
+});
