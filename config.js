599898c6406ae8f826d845917d7326ee963ac8ce
Michael Aufreiter <michael@substance.io>
2015-03-26 10:33:31 +0100

    try skipping some problematic articles from indexing

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 
 var config = {
 	// host: 'https://5vh18c43:yh79uny7q6gv658x@oak-9242088.eu-west-1.bonsai.io/'
-  host: 'https://38d8580ce27602d1000.qbox.io'
+  // host: 'https://38d8580ce27602d1000.qbox.io'
+  host: 'https://21c77g2u:5rpr7srwfn0ym748@aralia-9966923.eu-west-1.bonsai.io/'
 };
 
 console.log();
