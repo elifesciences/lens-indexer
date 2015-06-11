47c4f9526d6986befb6aeaba67ea31045580b2de
Michael Aufreiter <michael@substance.io>
2015-06-11 17:51:00 +0200

    Prevent empty entries from filelist.js.

--- scripts/02_create_list_of_urls.js
+++ scripts/02_create_list_of_urls.js
@@ -4,12 +4,17 @@ var fs = require('fs');
 var path = require('path');
 var request = require('request');
 var outputFile = path.join(__dirname, "..", "data", "filelist.js");
+var _ = require("underscore");
 var fileListUrl = "http://s3.amazonaws.com/elife-cdn/xml_files.txt";
+
+
+
 request(fileListUrl, function (error, response, fileList) {
   if (error || response.statusCode != 200) {
     console.error("Could not extract file-list from ", fileListUrl);
   } else {
     var urls = fileList.split(/\s+/);
+    urls = _.compact(urls);
     fs.writeFileSync(outputFile, "module.exports = " + JSON.stringify(urls, null, 2));
   }
 });
